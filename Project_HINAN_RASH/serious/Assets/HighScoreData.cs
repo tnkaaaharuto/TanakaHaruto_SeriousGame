5b9c163c38511ac45e06640b26d7cd98aa03e919
tnkaaaharuto <234533924+tnkaaaharuto@users.noreply.github.com>
2026-05-01 21:36:47 +0900

    プロジェクトフォルダ

--- Project_HINAN_RASH/serious/Assets/HighScoreData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "HighScoreData", menuName = "Scriptable Objects/HighScoreData")]
-public class HighScoreData : ScriptableObject
-{
-    public int stageNo;
-    public int stage1;
-    public int stage2;
-}
