589f6d8af2df68f4f806fb0480f555128194abea
tnkaaaharuto <234533924+tnkaaaharuto@users.noreply.github.com>
2026-05-01 21:17:55 +0900

    ビルドフォルダ

--- Project_HINAN_RASH/Assets/SoundData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SoundData", menuName = "Scriptable Objects/SoundData")]
-public class SoundData : ScriptableObject
-{
-    public byte se;
-    public byte bgm;
-}
